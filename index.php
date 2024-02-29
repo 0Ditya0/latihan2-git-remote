d7ae224dbe3ef27d3a71acd25fed354f3f748b8a
0Ditya0 <vaan0raditya0@gmail.com>
2024-02-29 21:21:53 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
     <p>Lorem ipsum dolor sit
     amet consectetur adipisicing elit.
     Id et aspernatur animi?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
